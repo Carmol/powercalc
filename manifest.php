0d5ba5ef1c73c25eab53a8fb1542ca245b55ae1d
Carmol <black-n-white@gmx.net>
2014-03-02 12:17:51 +0100

    Formatting
    Indents

--- manifest.php
+++ manifest.php
@@ -10,13 +10,13 @@ $hashes = "";
 $dir = new RecursiveDirectoryIterator(".");
 foreach(new RecursiveIteratorIterator($dir) as $file) {
 		
-		if (substr($file, 0, 3) != "./." && substr($file, 0, 8) != "./unused") {
-				if ($file->IsFile() && $file != "manifest.php" && substr($dir, 0, 3) != "./."
-						&& substr($file->getFilename(), 0, 1) != ".") {
-								echo $file . "\n";
-								$hashes .= md5_file($file);
-						}
-		}
+    if (substr($file, 0, 3) != "./." && substr($file, 0, 8) != "./unused") {
+	if ($file->IsFile() && $file != "manifest.php" && substr($dir, 0, 3) != "./."
+		&& substr($file->getFilename(), 0, 1) != ".") {
+	    echo $file . "\n";
+	    $hashes .= md5_file($file);
+	}
+    }
 }
 
 echo "# Hash: " . md5($hashes) . "\n";
