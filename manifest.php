d45ee1a438e3f172c18f5a13e88aaebd0435a14f
Carmol <black-n-white@gmx.net>
2014-03-02 12:19:05 +0100

    Double condition
    ./.

--- manifest.php
+++ manifest.php
@@ -11,8 +11,7 @@ $dir = new RecursiveDirectoryIterator(".");
 foreach(new RecursiveIteratorIterator($dir) as $file) {
 		
     if (substr($file, 0, 3) != "./." && substr($file, 0, 8) != "./unused") {
-	if ($file->IsFile() && $file != "manifest.php" && substr($dir, 0, 3) != "./."
-		&& substr($file->getFilename(), 0, 1) != ".") {
+	if ($file->IsFile() && $file != "manifest.php"&& substr($file->getFilename(), 0, 1) != ".") {
 	    echo $file . "\n";
 	    $hashes .= md5_file($file);
 	}
