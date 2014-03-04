e9f6a333b4ea95b8a277ffa5d53c86e868816c04
Carmol <black-n-white@gmx.net>
2014-03-04 19:15:24 +0100

    Nicer
    manifest.pl became a little bit nicer.

--- bin/manifest.pl
+++ bin/manifest.pl
@@ -3,11 +3,14 @@
 use strict;
 use warnings;
 
+use Readonly;
 use FCGI;
 use Carp;
 use File::Find;
 use Digest::MD5 qw( md5 md5_base64 );
 use Digest::MD5::File qw(dir_md5_hex file_md5_hex url_md5_hex);
+
+Readonly::Array my @ignore_items => qw( ../. ../bin ../README.md );
 
 sub list_dirs {
     my @dirs = shift;
@@ -20,14 +23,22 @@ sub list_dirs {
 
 sub must_be_shown {
     my $file = shift;
- 
-    # ugly hack; do not know, what I did wrong...
-    return 0 if $file =~ /^\.\.\/\./;
-    return 0 if $file =~ /^\.\.\/bin/;
-    return 0 if -d $file or $file eq '../manifest.php' or $file eq '../README.md'
-        or $file =~ /^\.\.\/bin/;
+
+    return 0 if -d $file;
+
+    for my $item (@ignore_items) {
+        return 0 if substr($file, 0, length($item)) eq $item;
+    }
 
     return 1;
+ 
+    ## ugly hack; do not know, what I did wrong...
+    #return 0 if $file =~ /^\.\.\/\./;
+    #return 0 if $file =~ /^\.\.\/bin/;
+    #return 0 if -d $file or $file eq '../manifest.php' or $file eq '../README.md'
+    #    or $file =~ /^\.\.\/bin/;
+
+    #return 1;
 }
 
 sub main {
