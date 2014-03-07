9410fb7a818b6850fc9cef845ee9ba5e0cd03fed
Carmol <black-n-white@gmx.net>
2014-03-07 13:07:03 +0100

    Formatting and playing around with vimrc

--- scripts/mycalcs.js
+++ scripts/mycalcs.js
@@ -14,150 +14,150 @@ var position;
 // Document stuff
 $(document).ready(function() {
 
-	window.addEventListener("onload", init, false);
-	//window.addEventListener("onbeforeunload", beforeUnLoad, false);
+    window.addEventListener("onload", init, false);
+    //window.addEventListener("onbeforeunload", beforeUnLoad, false);
 
-	init();
+    init();
 });
 
 function init() {
-	if (!localStorageAvailable) {
-		setStatus("Key value storage is not supported");
-		alert("Key value NOT storage supported");
-	}
-	else {
-		document.body.addEventListener("storage", storageHandler, false);
-		if (loadData()) {
-			setPower();
-		}
-		else {
-			alert("Data not loaded");
-		}
-	}
+    if (!localStorageAvailable) {
+        setStatus("Key value storage is not supported");
+        alert("Key value NOT storage supported");
+    }
+    else {
+        document.body.addEventListener("storage", storageHandler, false);
+        if (loadData()) {
+            setPower();
+        }
+        else {
+            alert("Data not loaded");
+        }
+    }
 }
 
 /*function beforeUnLoad() {
-	alert("Before unLoad()");
-}*/
+  alert("Before unLoad()");
+  }*/
 
 function loadData() {
-	weight = checkRange(localStorage.weight);
-	speed = checkRange(localStorage.speed);
-	inclination = checkRange(localStorage.inclination);
-	crr = checkRange(localStorage.crr);
-	position = localStorage.position;
-	
-	$('#weightNumber').text(weight);
+    weight = checkRange(localStorage.weight);
+    speed = checkRange(localStorage.speed);
+    inclination = checkRange(localStorage.inclination);
+    crr = checkRange(localStorage.crr);
+    position = localStorage.position;
+
+    $('#weightNumber').text(weight);
     // Here we just have to change the input type to something more
     // suitable.
-	$('#dynWeight').html('<input type="range" min="40" max="150" id="weight"'
+    $('#dynWeight').html('<input type="range" min="40" max="150" id="weight"'
         + ' onChange="setValues()" value="' + weight + '" step="5"/>');
-		
-	$('#inclinationNumber').text(inclination);
-	$('#dynInclination').html('<input type="range" min="-5" max="15" id="inclination"'
-        + ' onChange="setValues()" value="' + inclination + '" step="0.5" />');
-		
-	$('#speedNumber').text(speed);
-	$('#dynSpeed').html('<input type="range" min="5" max="50" id="speed" '
+
+    $('#inclinationNumber').text(inclination);
+    $('#dynInclination').html('<input type="range" min="0" max="15" id="inclination"'
+        + ' onChange="setValues()" value="' + inclination + '" step="1.0" />');
+
+    $('#speedNumber').text(speed);
+    $('#dynSpeed').html('<input type="range" min="5" max="50" id="speed" '
         + ' onChange="setValues()" value="' + speed + '" step="1"/>');
-		
-	$('#crrNumber').text(crr);
-	$('#dynCrr').html('<input type="range" min="0.002" max="0.004" step="0.0005" id="crr"'
+
+    $('#crrNumber').text(crr);
+    $('#dynCrr').html('<input type="range" min="0.002" max="0.004" step="0.0005" id="crr"'
         + ' onChange="setValues()" value="' + crr + '" width="300" height="10" />');
-		
-	var positionChoiceHTML = '<select name="position" id="position"'
+
+    var positionChoiceHTML = '<select name="position" id="position"'
         + ' onChange="setPower()">';
-	
-	positionChoiceHTML += (position == "up"
+
+    positionChoiceHTML += (position == "up"
         ? '<option value="up" selected>Upright</option>'
         : '<option value="up">Upright</option>');
-	positionChoiceHTML += (position == "brake_levers"
+    positionChoiceHTML += (position == "brake_levers"
         ? '<option value="brake_levers" selected>Brake levers</option>'
         : '<option value="brake_levers">Brake levers</option>');
-	positionChoiceHTML += (position == "bend"
+    positionChoiceHTML += (position == "bend"
         ? '<option value="bend" selected>Bend</option>'
         : '<option value="bend">Bend</option>');
-	positionChoiceHTML += "</select>"	
-	$('#dynSpan').html(positionChoiceHTML);
-		
-	$('#power_title').text(calculatePower(weight, speed, inclination, crr, position)
-	+ " w").show();
+    positionChoiceHTML += "</select>"
+        $('#dynSpan').html(positionChoiceHTML);
 
-	return true;
+    $('#power_title').text(calculatePower(weight, speed, inclination, crr, position)
+        + " w").show();
+
+    return true;
 }
 
 // ******************************************************************
 // Housekeeping of data and values
 function checkRange(value) {
-	if (!value || !isNumber(value)) value = 0;
+    if (!value || !isNumber(value)) value = 0;
 
-	return value;
+    return value;
 }
 
-function storeValues() {  
-	localStorage.setItem("weight", weight);
-	localStorage.setItem("inclination", inclination);
-	localStorage.setItem("speed", speed);
-	localStorage.setItem("crr", crr);
-	localStorage.setItem("position", position);
+function storeValues() {
+    localStorage.setItem("weight", weight);
+    localStorage.setItem("inclination", inclination);
+    localStorage.setItem("speed", speed);
+    localStorage.setItem("crr", crr);
+    localStorage.setItem("position", position);
 
-	return true;
+    return true;
 }
 
 function storageHandler(event) {
-	var info = "A storage event occurred [" + 
-	"url=" + event.url + ", " +
-	"key=" + event.key + ", " +
-	"new value=" + event.event.newValue + ", " +
-	"old value=" + event.event.oldValue + ", " +
-	"window=" + event.event.window+ "]" + "";
-			
-	setStatus(info);
+    var info = "A storage event occurred [" +
+        "url=" + event.url + ", " +
+        "key=" + event.key + ", " +
+        "new value=" + event.event.newValue + ", " +
+        "old value=" + event.event.oldValue + ", " +
+        "window=" + event.event.window+ "]" + "";
+
+    setStatus(info);
 }
 
 function setStatus(statusText) {
-	var para = document.createElement("p");
-	para.appendChild(document.createTextNode(statusText));
-	document.getElementById("statusDiv").appendChild(para);
+    var para = document.createElement("p");
+    para.appendChild(document.createTextNode(statusText));
+    document.getElementById("statusDiv").appendChild(para);
 }
 
 function setParameters() {
-	weight      = $('#weight').val();
-	inclination = $('#inclination').val()
-	speed       = $('#speed').val();
-	crr         = $('#crr').val();
-	position    = $('#position').val();
+    weight      = $('#weight').val();
+    inclination = $('#inclination').val();
+    speed       = $('#speed').val();
+    crr         = $('#crr').val();
+    position    = $('#position').val();
 }
 
 
 function getPositionIndex(pos) {
 
     if (position == "up") return 0;
-	if (position == "brake_levers") return 1;
-	if (position == "bend") return 2;
+    if (position == "brake_levers") return 1;
+    if (position == "bend") return 2;
 }
 
 
 function setValues() {
-	
-	$('#weightNumber').text(weight);
-	$('#inclinationNumber').text(inclination);
-	$('#crrNumber').text(crr);
-	$('#speedNumber').text(speed);
-	
-	setPower();
 
-	$('#weightNumber').text(weight);
-	$('#inclinationNumber').text(inclination);
-	$('#crrNumber').text(crr);
-	$('#speedNumber').text(speed);
+    $('#weightNumber').text(weight);
+    $('#inclinationNumber').text(inclination);
+    $('#crrNumber').text(crr);
+    $('#speedNumber').text(speed);
+
+    setPower();
+
+    $('#weightNumber').text(weight);
+    $('#inclinationNumber').text(inclination);
+    $('#crrNumber').text(crr);
+    $('#speedNumber').text(speed);
 }
 
 function setPower() {
     setParameters();
 
-    $('#value').text(calculatePower(weight, speed, inclination, crr, position)
-            + " w").show();
+    //$('#value').text(calculatePower(weight, speed, inclination, crr, position)
+    //        + " w").show();
     $('#position').selectedIndex = getPositionIndex(position);
     $('#power_title').text(calculatePower(weight, speed, inclination, crr, position)
             + " w").show();
@@ -167,12 +167,12 @@ function setPower() {
 
 function isNumber(toCheck) {
 
-	if (/[0-9\.\-]/.test(toCheck)) {
-		return true;
-	}
-	else {
-		return false;
-	}
+    if (/[0-9\.\-]/.test(toCheck)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 // The math
@@ -206,11 +206,11 @@ function pAcceleration(weight, spd, grade, crr, position) {
 // ******************************************************************
 function calculatePower(weight, spd, inclination, crr, position) {
 
-	var calc_speed = 1000 * spd / 3600;
-	var calc_inclination = inclination / 100; // percent
-	
-	return Number((pRollingResistance(weight, calc_speed, calc_inclination, crr, position)
-	+ pWind(weight, calc_speed, calc_inclination, crr, position)
-	+ pGravity(weight, calc_speed, calc_inclination, crr, position)
-	+ pAcceleration(weight, calc_speed, calc_inclination, crr, position)).toFixed(0));
+    var calc_speed = 1000 * spd / 3600;
+    var calc_inclination = inclination / 100; // percent
+
+    return Number((pRollingResistance(weight, calc_speed, calc_inclination, crr, position)
+                + pWind(weight, calc_speed, calc_inclination, crr, position)
+                + pGravity(weight, calc_speed, calc_inclination, crr, position)
+                + pAcceleration(weight, calc_speed, calc_inclination, crr, position)).toFixed(0));
 }
